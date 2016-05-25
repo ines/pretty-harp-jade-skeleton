465f8a2e20df67bcc130b0d913e12bd0974c4dd2
Ines Montani <ines.montani@gmail.com>
2016-05-25 22:33:38 +0200

    Add files

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+//  JavaScript
+//  =========================
+//  Your custom JavaScript goes here. If you don't need any, simply delete this
+//  file and remove the script tag from _layout.jade.
+//  =========================
+
+console.log('Hello world!');
